7912f84ce059b25d2fab8033e259c2c390c54874
AlexandrAguilarC <alexandra10aguilarc@gmail.com>
2023-08-23 13:01:44 -0400

    Métodos para redireccionar vistas al inicio y cierre de sesión.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    #Al ingresar un usuario debe ser redirigido a la vista de articles
+    def after_sign_in_path_for(resource)
+        articles_path
+    end
+
+    #Al cerrar session el usuario es redirigido a inicia sesión
+    def after_sign_out_path_for(resource_or_scope)
+        new_user_session_path
+    end
 end
